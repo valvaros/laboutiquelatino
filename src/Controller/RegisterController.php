345d485501f07c3d420ca4603d2a4d9ca3facfe9
valvaros <mathieu.guezennec@hotmail.com>
2022-04-04 10:07:58 +0200

    update hashencoder

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -31,11 +31,9 @@ class RegisterController extends AbstractController
 
         if($form->isSubmitted()&& $form->isValid()){
 
-            $user = $form->getData();
-
             $password = $encoder->hashPassword($user,$user->getPassword());
 
-            
+            $user->setPassword($password);
             
             
             $this->entityManager->persist($user);
